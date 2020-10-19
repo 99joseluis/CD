645185e463f8cd1dbc3ae6772e1b6ab3daebca87
99joseluis <jose_luis@ciencias.unam.mx>
2020-10-19 00:42:12 -0500

    Modificando el objeto nodo

--- Practica1/src/Nodo.py
+++ Practica1/src/Nodo.py
@@ -1,5 +1,5 @@
 import simpy
-
+from Canales.Canal import Canal
 class Nodo:
     """Representa un nodo.
 
@@ -9,6 +9,36 @@ class Nodo:
     def __init__(self, id_nodo: int, vecinos: list, canal_entrada: simpy.Store,
                  canal_salida: simpy.Store):
         '''Inicializa los atributos del nodo.'''
+        print("se inicializa el nodo")
+        self.id_nodo = id_nodo
+        self.vecinos = vecinos
+        self.canal_entrada = canal_entrada
+        self.canal_salida = canal_salida
+        self.mensaje = False
 
-    def get_id(self) -> int:
-        '''Regresa el id del nodo.'''
+
+    def algo1(self):
+        print("Entra el proceso algo1")
+        if self.id_nodo == 0:
+            #Solo la raiz envia el primer mensaje 
+            print("Enviando el primer mensaje (raiz)")
+            self.mensaje = True
+            self.canal_salida.envia(self.id_nodo, self.vecinos)
+        else:
+            while true:
+                if self.mensaje is not True:
+                    msj = yield self.canal_entrada.get()
+                    print("Mensaje get = ")
+                    print(msj)
+                    self.mensaje = msj
+
+                    #print("%d recibío el mensaje de %d " %(self.id_nodo, msj))
+                    # lo reenviamos a los vecinos
+                    self.canal_salida.envia(self.id_nodo, self.vecinos)
+                    print("Canal de entrada : ")
+                    print(self.canal_entrada.canales)
+                    print("Canal de salida :")
+                    print(self.canal_salida.canales)
+                    break
+
+
