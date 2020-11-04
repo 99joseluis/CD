c65c2773546add20994319ad0275b65d86e79a96
antonioreyesmtz21 <54083718+antonioreyesmtz21@users.noreply.github.com>
2020-11-04 00:09:42 -0600

    Update NodoDFS.py

--- Practica02/src/NodoDFS.py
+++ Practica02/src/NodoDFS.py
@@ -9,10 +9,96 @@ class NodoDFS(Nodo):
     ''' Implementa la interfaz de Nodo para el algoritmo de Broadcast.'''
     def __init__(self, id_nodo, vecinos, canal_entrada, canal_salida):
         ''' Constructor de nodo que implemente el algoritmo DFS. '''
-        # Tu implementación va aquí
-        pass
+        self.id_nodo = id_nodo
+        self.vecinos = vecinos
+        self.canal_entrada = canal_entrada
+        self.canal_salida = canal_salida
+        # Atributos extra
+        self.padre = id_nodo
+        self.hijos = []
 
-    def bfs(self, env):
+    def vecinoMenor(self,visitados):
+        ''' Funcion para obtener el menor de los vecinos'''
+        dif = list(set(self.vecinos).difference(visitados))
+        r = dif[0]
+
+        for n in dif:
+            if n < r:
+                r = n 
+
+        return r
+
+    def get_id(self):
+        return self.id_nodo
+
+    def dfs(self, env):
         ''' Algoritmo DFS. '''
-        # Tu implementación va aquí
-        pass
+        
+        if self.id_nodo == 0:
+
+            yield env.timeout(TICK)
+            self.canal_salida.envia(['G',self.id_nodo,{0}],[self.vecinoMenor(set())])
+            self.hijos.append(self.vecinoMenor(set()))
+
+        while True:
+            msg = yield self.canal_entrada.get()
+
+            tipo_msg = msg[0]
+            emisor = msg[1]
+            visitados = msg[2]
+
+            if (tipo_msg == 'G'):
+
+                self.padre = emisor
+
+                if (set(self.vecinos).issubset(visitados)):
+
+                    yield env.timeout(TICK)
+                    self.canal_salida.envia(['B',self.id_nodo,visitados.union({self.id_nodo})],[emisor])
+                    self.hijos = []
+                    
+                else:
+
+                    k = self.vecinoMenor(visitados)       
+                    yield env.timeout(TICK)
+                    self.canal_salida.envia(['G',self.id_nodo,visitados.union({self.id_nodo})],[k])
+                    self.hijos = [k]
+
+            elif (tipo_msg == 'B'):
+
+                if (set(self.vecinos).issubset(visitados)):
+
+                    if (self.padre == self.id_nodo):
+
+                        print("El algoritmo terminó")
+
+                    else:
+
+                        yield env.timeout(TICK)
+                        self.canal_salida.envia(['B',self.id_nodo,visitados],[self.padre])
+                        
+                else:
+
+                    k = self.vecinoMenor(visitados)
+                    yield env.timeout(TICK)
+                    self.canal_salida.envia(['G',self.id_nodo,visitados],[k])
+                    self.hijos = list(set (self.hijos).union({k}))
+                    
+if __name__ == "__main__":
+   env = simpy.Environment()
+   bc_pipe = CanalRecorridos(env)
+
+   # La lista que representa la gráfica
+   grafica = []
+   adyacencias = [[1, 3, 4, 6], [0, 3, 5, 7], [3, 5, 6], [0, 1, 2], [0], [1, 2], [0, 2], [1]]
+
+
+   # Creamos los nodos
+   for i in range(0, len(adyacencias)):
+       grafica.append(NodoDFS(i, adyacencias[i],
+                                bc_pipe.crea_canal_de_entrada(), bc_pipe))
+
+   # Le decimos al ambiente lo que va a procesar ...
+   for nodo in grafica:
+       env.process(nodo.dfs(env))
+       # ...y lo corremos            
